17927fc9d6c92e53609916b197b19e6f5eaaad7c
Alexius <alexiusdevox@outlook.com>
2025-03-27 01:24:02 +0100

    Update signout.tsx

--- app/signout.tsx
+++ app/signout.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export default function SignoutButton() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+    router.push("/");
+    router.refresh(); // Aggiorna la pagina per riflettere il nuovo stato di autenticazione
+  };
+
   return (
     <button
       className="bg-neutral-700 text-white p-2 rounded-md"
-      onClick={() => authClient.signOut()}
+      onClick={handleSignOut}
     >
       Signout
     </button>
