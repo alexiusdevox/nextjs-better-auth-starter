2beb12405e957df1e3a844357d45a4418bee0043
Alexius <alexiusdevox@outlook.com>
2025-03-27 01:24:35 +0100

    Update page.tsx
    asd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { auth } from "@/lib/auth";
-import { signIn, signUp } from "@/server/users";
 import { headers } from "next/headers";
 import SignOut from "./signout";
+import SignUpButton from "./signout";
+import LoginButton from "./login-button";
+
 
 export default async function Home() {
   const session = await auth.api.getSession({
@@ -11,19 +13,14 @@ export default async function Home() {
   return (
     <main className="flex flex-col gap-3 items-center justify-center p-10">
       <div className="flex gap-3">
-        <button
-          className="bg-neutral-700 text-white p-2 rounded-md"
-          onClick={signIn}
-        >
-          Sign In
-        </button>
-        <button
-          className="bg-neutral-700 text-white p-2 rounded-md"
-          onClick={signUp}
-        >
-          Sign Up
-        </button>
-        <SignOut />
+        {!session ? (
+          <>
+            <LoginButton />
+            <SignUpButton />
+          </>
+        ) : (
+          <SignOut />
+        )}
       </div>
       <p>{!session ? "Not authenticated" : session.user.name}</p>
     </main>
